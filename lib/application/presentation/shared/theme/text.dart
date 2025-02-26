348d3f3a4b9263df88d037a735e3fb1158796fe4
Khwan <khwan.nontawichit@gmail.com>
2025-02-26 23:26:12 +0700

    [UPDATE] theme text

--- lib/application/presentation/shared/theme/text.dart
+++ lib/application/presentation/shared/theme/text.dart
@@ -5,14 +5,24 @@ class AppTextTheme {
 
   static TextTheme light = TextTheme(
     headlineLarge: _baseStyle(24, FontWeight.bold, Colors.black),
+    headlineSmall: _baseStyle(20, FontWeight.bold, Colors.black),
+    titleLarge: _baseStyle(18, FontWeight.w600, Colors.black),
     titleMedium: _baseStyle(16, FontWeight.w600, Colors.black),
+    titleSmall: _baseStyle(14, FontWeight.w600, Colors.black),
+    bodyLarge: _baseStyle(16, FontWeight.normal, Colors.black),
     bodyMedium: _baseStyle(14, FontWeight.normal, Colors.black),
+    bodySmall: _baseStyle(12, FontWeight.normal, Colors.black),
   );
 
   static TextTheme dark = TextTheme(
     headlineLarge: _baseStyle(24, FontWeight.bold, Colors.white),
+    headlineSmall: _baseStyle(20, FontWeight.bold, Colors.white),
+    titleLarge: _baseStyle(18, FontWeight.w600, Colors.white),
     titleMedium: _baseStyle(16, FontWeight.w600, Colors.white),
+    titleSmall: _baseStyle(14, FontWeight.w600, Colors.white),
+    bodyLarge: _baseStyle(16, FontWeight.normal, Colors.white),
     bodyMedium: _baseStyle(14, FontWeight.normal, Colors.white),
+    bodySmall: _baseStyle(12, FontWeight.normal, Colors.white),
   );
 
   static TextStyle _baseStyle(double size, FontWeight weight, Color color) {
